711adf19ba53faa2c55c96f6136be543ca167693
md afsar <mdafsaransal@gmail.com>
2022-10-11 00:08:46 +0530

    added code

---
+++ jav1.js
@@ -0,0 +1,11 @@
+// //Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+
+let name="Deadpool";
+let age=59;
+
+console.log(name);
+console.log(typeof(age));
+
---
+++ report.js
@@ -0,0 +1,25 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols 
+let report="𝑹𝑬𝑷𝑶𝑹𝑻"
+
+let name="Raju";
+let school="abc school";
+let grade="6th";
+let section="A";
+let rollno=25;
+
+let maths=78;
+let english=67;
+let science=70;
+
+
+console.log(report)
+console.log("Name-> ",name)
+console.log("School-> ",school)
+console.log("Grade-> ",grade)
+console.log("Section-> ",section)
+console.log("Rollno-> ",rollno)
+console.log("Math-> ",maths)
+console.log("Physics-> ",english)
+console.log("Chemistry-> ",science)
+          
